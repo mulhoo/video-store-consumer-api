ae03269cf288e6dccdf2032cdf86b32a910ee144
Maryam Heshmati <mairheshmati@gmail.com>
2020-06-24 12:09:22 -0700

    added created action for movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,17 +11,35 @@ class MoviesController < ApplicationController
     render status: :ok, json: data
   end
 
+  def create
+    movie = Movie.new(movie_params)
+
+    if movie.save
+      render json: movie.as_json(only: [:id, :title, :overview, :release_date, :image_url, :external_id]), status: :created
+      return
+    else
+      render json: {
+        errors: movie.errors.messages,
+      }, status: :bad_request
+      return
+    end
+  end
+
   def show
     render(
       status: :ok,
       json: @movie.as_json(
         only: [:title, :overview, :release_date, :inventory],
-        methods: [:available_inventory]
-        )
-      )
+        methods: [:available_inventory],
+      ),
+    )
   end
 
   private
+
+  def movie_params
+    return params.permit(:title, :overview, :release_date, :image_url, :external_id)
+  end
 
   def require_movie
     @movie = Movie.find_by(title: params[:title])
