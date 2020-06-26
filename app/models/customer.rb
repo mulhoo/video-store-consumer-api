9cc7b31758b103fa3c033cee6e99640e8f5781f1
mulhoo <oliviamulholland94@gmail.com>
2020-06-26 01:33:06 -0700

    added validations for uniqueness

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,8 @@ class Customer < ApplicationRecord
   has_many :rentals
   has_many :movies, through: :rentals
 
+  validates :name, :presence => true, :uniqueness => true
+
   def movies_checked_out_count
     self.rentals.where(returned: false).length
   end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ApplicationRecord
   has_many :rentals
   has_many :customers, through: :rentals
+  validates :title, :presence => true, :uniqueness => true
 
   def available_inventory
     self.inventory - Rental.where(movie: self, returned: false).length
